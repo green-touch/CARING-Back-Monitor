3b073a1144bc729edca31ad13dcc9f11d47d11f7
hann <wjdgks1233@gmail.com>
2025-03-08 15:14:01 +0900

    init commit

---
+++ src/test/java/com/caring/monitoring_service/MonitoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caring.monitoring_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
