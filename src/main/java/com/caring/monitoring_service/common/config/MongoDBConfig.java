6458c750a787039f54db223805814dca8c3e5792
hann <wjdgks1233@gmail.com>
2025-03-08 15:45:22 +0900

    [CARE-59] feature : create MongoDBConfig.class #3

---
+++ src/main/java/com/caring/monitoring_service/common/config/MongoDBConfig.java
@@ -0,0 +1,9 @@
+package com.caring.monitoring_service.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoDBConfig {
+}
