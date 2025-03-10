7c80980f3bc1b77f202d9f9a6774a265fd3df96e
hann <wjdgks1233@gmail.com>
2025-03-10 23:51:21 +0900

    [CARE-60] refactor : feedback by rabbit(edit for JsonParsingException) #5

--- src/main/java/com/caring/monitoring_service/common/service/kafka/KafkaLogProducer.java
+++ src/main/java/com/caring/monitoring_service/common/service/kafka/KafkaLogProducer.java
@@ -1,11 +1,14 @@
 package com.caring.monitoring_service.common.service.kafka;
 
 import com.caring.monitoring_service.common.util.StaticVariable;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.HashMap;
 
 import static com.caring.monitoring_service.common.util.StaticVariable.KAFKA_LOG_TOPIC;
 
@@ -13,16 +16,27 @@ import static com.caring.monitoring_service.common.util.StaticVariable.KAFKA_LOG
 @Service
 public class KafkaLogProducer {
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper= new ObjectMapper();
 
     public KafkaLogProducer(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void sendLog(String userUuid, String message) {
-        String logMessage = String.format("{\"userUuid\":\"%s\",  \"message\":\"%s\"}",
-                userUuid, message);
+        HashMap<String, String> logMap = new HashMap<>();
+        logMap.put("userUuid", userUuid);
+        logMap.put("message", message);
 
-        kafkaTemplate.send(KAFKA_LOG_TOPIC, userUuid, logMessage);
-        log.info("✅ Log sent: {}", logMessage);
+        String logMessage;
+        try{
+            logMessage = objectMapper.writeValueAsString(logMap);
+            kafkaTemplate.send(KAFKA_LOG_TOPIC, userUuid, logMessage);
+            log.info("✅ Log sent: {}", logMessage);
+        } catch (JsonProcessingException e){
+            log.error("❌ JSON 파싱 오류: {}", e.getMessage());
+        }
+        catch (Exception e) {
+            log.error("❌ Failed to process log: {}", e.getMessage());
+        }
     }
 }
