b399caf3ac786f0c8de8a4e098373ab9e69368aa
hann <wjdgks1233@gmail.com>
2025-05-06 12:56:58 +0900

    [CARE-92] feat : fill up method contents in saveLog #7

--- src/main/java/com/caring/monitoring_service/domains/log/business/service/UserLogDomainService.java
+++ src/main/java/com/caring/monitoring_service/domains/log/business/service/UserLogDomainService.java
@@ -1,5 +1,7 @@
 package com.caring.monitoring_service.domains.log.business.service;
 
+import com.caring.monitoring_service.presentation.log.vo.RequestUserLog;
+
 public interface UserLogDomainService {
-    String saveLog(RequestLogDto requestLogDto);
+    String saveLog(RequestUserLog requestUserLog);
 }
--- src/main/java/com/caring/monitoring_service/domains/log/business/service/UserLogDomainServiceImpl.java
+++ src/main/java/com/caring/monitoring_service/domains/log/business/service/UserLogDomainServiceImpl.java
@@ -1,15 +1,23 @@
 package com.caring.monitoring_service.domains.log.business.service;
 
 import com.caring.monitoring_service.common.annotation.DomainService;
+import com.caring.monitoring_service.domains.log.dao.entity.UserLog;
 import com.caring.monitoring_service.domains.log.dao.repository.UserLogRepository;
+import com.caring.monitoring_service.presentation.log.vo.RequestUserLog;
 import lombok.RequiredArgsConstructor;
 
 @DomainService
 @RequiredArgsConstructor
 public class UserLogDomainServiceImpl implements UserLogDomainService {
+
     private final UserLogRepository userLogRepository;
+
     @Override
-    public String saveLog(RequestLogDto requestLogDto) {
-        return "";
+    public String saveLog(RequestUserLog requestUserLog) {
+        UserLog userLog = UserLog.builder()
+                .userUuid(requestUserLog.getUserUuid())
+                .message(requestUserLog.getMessage())
+                .build();
+        return userLogRepository.save(userLog).getId();
     }
 }
