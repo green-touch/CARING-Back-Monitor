3b4b3ea169b59fb28029129c5e0ff1a5719bc901
hann <wjdgks1233@gmail.com>
2025-05-06 14:36:25 +0900

    [CARE-92] feat : create docker-compose.yml for run kafka & zookeeper #7

--- src/main/java/com/caring/monitoring_service/presentation/log/vo/RequestUserLog.java
+++ src/main/java/com/caring/monitoring_service/presentation/log/vo/RequestUserLog.java
@@ -1,14 +1,27 @@
 package com.caring.monitoring_service.presentation.log.vo;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @Builder
-@AllArgsConstructor
 public class RequestUserLog {
-    private String id;
-    private String userUuid;
-    private String message;
+    private final String id;
+    private final String userUuid;
+    private final String message;
+
+    @JsonCreator
+    public RequestUserLog(
+            @JsonProperty("id") String id,
+            @JsonProperty("userUuid") String userUuid,
+            @JsonProperty("message") String message
+    ) {
+        this.id = id;
+        this.userUuid = userUuid;
+        this.message = message;
+    }
 }
