20b82cd0526ac80155702acba4c924159bcabff5
hann <wjdgks1233@gmail.com>
2025-05-14 16:23:14 +0900

    refactor : use jacksonized annotation #92

--- src/main/java/com/caring/monitoring_service/presentation/log/vo/RequestUserLog.java
+++ src/main/java/com/caring/monitoring_service/presentation/log/vo/RequestUserLog.java
@@ -6,22 +6,24 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
 
 @Data
 @Builder
+@Jacksonized
 public class RequestUserLog {
     private final String id;
     private final String userUuid;
     private final String message;
 
-    @JsonCreator
-    public RequestUserLog(
-            @JsonProperty("id") String id,
-            @JsonProperty("userUuid") String userUuid,
-            @JsonProperty("message") String message
-    ) {
-        this.id = id;
-        this.userUuid = userUuid;
-        this.message = message;
-    }
+//    @JsonCreator
+//    public RequestUserLog(
+//            @JsonProperty("id") String id,
+//            @JsonProperty("userUuid") String userUuid,
+//            @JsonProperty("message") String message
+//    ) {
+//        this.id = id;
+//        this.userUuid = userUuid;
+//        this.message = message;
+//    }
 }
